0db8ec820624842be39179d69285978aa4a4f839
t29mato <tomoya.matou@gmail.com>
2018-08-11 22:45:30 +0900

    commit

--- www/js/app.js
+++ www/js/app.js
@@ -1,158 +1,65 @@
-// Youtube Iframe APIの呼び出し
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-var json;
-var currentVideo = 0;
+var videoPlayer;
+var jsonVideo;
+var currentVideo;
 var tag;
+var NXB_API_URL = 'https://script.google.com/macros/s/AKfycbw6IbZpgyHknQ4ARKAzNEGv4bVwthZBoYExxQZSOxoKfdds_8Q/exec';
 
 function onYouTubeIframeAPIReady() {
-  $.ajax({
-    url: 'https://script.google.com/macros/s/AKfycbxJOWoWTM1lKPXX0J1aXAXgBHA1jBOFDaHQ1JLPeLrArbYsLGe9/exec',
-    type: 'GET'
+  getVideo().done(function(data) {
+    jsonVideo = JSON.parse(data);
+    createVideoPlayer(0);
+    createVideoList();
   })
-  .done((data) => {
-    console.log("succeded for getting data");
-    json = JSON.parse(data);
-    $('#title').append(' [' + json.length + ']');
-    for (var i in json) {
-      var a = document.createElement('a');
-      a.href = '#';
-      a.setAttribute('onclick', 'loadPlayer(' + i + ')');
-      a.classList.add('relative');
-      var img = document.createElement('img');
-      img.src = json[i].thumbnail;
-      img.classList.add('thumbnail');
-      a.append(img);
-      $('#video-list').append(a); 
-    }
-    // loadPlayer(0);
-  })
-  .fail((data) => {
-    console.log("failed for getting data");
-  })
-  .always((data) => {
-  });
 }
 
-function updateVideo(tag) {
-  document.querySelector('#menu').close();
-  document.querySelector('#info').close();
-  $('#video-list').empty();
-  $('#title').text(tag);
-  $.ajax({
-    url: 'https://script.google.com/macros/s/AKfycbxJOWoWTM1lKPXX0J1aXAXgBHA1jBOFDaHQ1JLPeLrArbYsLGe9/exec?tag=' + tag,
-    type: 'GET'
+function getVideo(tag) {
+  var url = NXB_API_URL;
+  if (tag) { url = url + '?tag=' + tag; }
+  return $.ajax({
+    url: url,
+    type: 'GET',
   })
-  .done((data) => {
-    console.log("succeded for getting data");
-    json = JSON.parse(data);
-    $('#title').append(' [' + json.length + ']');
-    for (var i in json) {
-      var a = document.createElement('a');
-      a.href = '#';
-      a.setAttribute('onclick', 'loadPlayer(' + i + ')');
-      a.classList.add('relative');
-      var img = document.createElement('img');
-      img.src = json[i].thumbnail;
-      img.classList.add('thumbnail');
-      a.append(img);
-      $('#video-list').append(a); 
-      // $('#video-list img').eq(i).attr('src', json[i].thumbnail);
-    }
-    // loadPlayer(0);
-  })
-  .fail((data) => {
-    console.log("failed for getting data");
-  })
-  .always((data) => {
-  });
 }
 
+function updateVideoList(tag) {
+  emptyVideoList();
+  getVideo(tag);
+}
 
-$("#video-list > a").click(function () {
-  console.log("from click a");
-  loadPlayer($(this).attr("rel"));
-});
-
-
-function loadPlayer(id) {
-  $('.loader').remove();
-  $('<img>', {
-    class: 'loader',
-    src: 'img/loader_pause.gif',
-  }).appendTo($('#video-list a').eq(id));
-
-  $('#ons-list-info').empty();
-  var html = $('#ons-list-info');
-  html.append('<ons-list-item>Playing Video Info.</ons-list-item>');
-  if (json[id].title) {
-    html.append('<ons-list-header class="menu_header">Title</ons-list-header>');
-    html.append('<ons-list-item>' + json[id].title + '</ons-list-item>');
-  }
-  if (json[id].year) {
-    html.append('<ons-list-header class="menu_header">Filming Date</ons-list-header>');
-    html.append('<ons-list-item>' + json[id].year + '/' + json[id].month + '</ons-list-item>');
-  }
-  if (json[id].creator_name_en) {
-    html.append('<ons-list-header class="menu_header">Creator</ons-list-header>');
-    var item = $('<ons-list-item>', {
-      tappable: '',
-      onclick: 'updateVideo("' + json[id].creator_name_en + '")',
+function createVideoList() {
+  for (var i in jsonVideo) {
+    var a = $('<a>', {
+      href: '#',
+      onclick: 'loadVideo(' + i + ')',
+      class: 'relative',
     });
-    item.text(json[id].creator_name_en);
-    html.append(item);
-  }
-
-  html.append('<ons-list-header class="menu_header">Tag</ons-list-header>');
-  var tags = [];
-  if (json[id].country_name_en) {
-    tags.push(json[id].country_name_en);
-  }
-  if (json[id].area_name_en) {
-    tags.push(json[id].area_name_en);
-  }
-  if (json[id].spot_name_en) {
-    tags.push(json[id].spot_name_en);
-  }
-  if (json[id].sea_name_en) {
-    tags.push(json[id].sea_name_en);
-  }
-  if (json[id].other_name_en) {
-    tags.push(json[id].other_name_en);
-  }
-  for (var tag of tags) {
-    var item = $('<ons-list-item>', {
-      tappable: '',
-      onclick: 'updateVideo("' + tag + '")',
+    var img = $('<img>', {
+      src: jsonVideo[i].thumbnail,
+      class: 'thumbnail',
     });
-    item.text(tag);
-    html.append(item);    
+    a.html(img);
+    $('#video_list').append(a);
   }
-  
-  currentVideo = id;
-  console.log("loading video" + json[id].preview_url);
-  console.log(json[id].end);
-  if (player) {
-    player.destroy();
-  }
-  player = new YT.Player('video-player', {
+}
+
+function emptyVideoList() {
+  $('#video_list').empty();
+}
+
+function createVideoPlayer(num) {
+  console.log(num);  
+  console.log(jsonVideo[0]);  
+  videoPlayer = new YT.Player('video_player', {
     height: '210',
     width: '100%',
-    videoId: json[id].video_id,
+    videoId: jsonVideo[num].video_id,
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
     },
     playerVars: {
-      start: json[id].start,
-      end: json[id].end,
-      rel: 0,
+      start: jsonVideo[num].start,
+      end: jsonVideo[num].end,
       showinfo: 0,
       playsinline: 1,
       controls: 1,
@@ -163,18 +70,16 @@ function loadPlayer(id) {
   });
 }
 
-// 4. The API will call this function when the video player is ready.
+function destroyVideoPlayer() {
+  if (videoPlayer) { videoPlayer.destroy(); }
+}
+
 function onPlayerReady(event) {
   event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
 function onPlayerStateChange(event) {
-  // 再生した時
-  if (event.data == YT.PlayerState.PLAYING && !done) {
+  if (event.data == YT.PlayerState.PLAYING) {
     $('.loader').attr('src','img/loader.gif');    
   } else if (event.data == YT.PlayerState.ENDED) {
     nextVideo();
