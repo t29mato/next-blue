e18f32752612f8d6669c28ac6a47324cf52d3749
t29mato <tomoya.matou@gmail.com>
2018-08-11 19:37:25 +0900

    second commit

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,5 @@
-// 2. This code loads the IFrame Player API code asynchronously.
+// Youtube Iframe APIの呼び出し
 var tag = document.createElement('script');
-
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
@@ -10,14 +9,17 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 var json;
 var currentVideo = 0;
+var tag;
+
 function onYouTubeIframeAPIReady() {
   $.ajax({
     url: 'https://script.google.com/macros/s/AKfycbxJOWoWTM1lKPXX0J1aXAXgBHA1jBOFDaHQ1JLPeLrArbYsLGe9/exec',
     type: 'GET'
   })
   .done((data) => {
-    console.log("succeded for getting data")
+    console.log("succeded for getting data");
     json = JSON.parse(data);
+    $('#title').append(' [' + json.length + ']');
     for (var i in json) {
       var a = document.createElement('a');
       a.href = '#';
@@ -27,16 +29,51 @@ function onYouTubeIframeAPIReady() {
       img.src = json[i].thumbnail;
       img.classList.add('thumbnail');
       a.append(img);
-      $('#video-list').append(a);          
+      $('#video-list').append(a); 
     }
-    loadPlayer(0);
+    // loadPlayer(0);
   })
   .fail((data) => {
-    console.log("failed for getting data")
+    console.log("failed for getting data");
   })
   .always((data) => {
   });
 }
+
+function updateVideo(tag) {
+  document.querySelector('#menu').close();
+  document.querySelector('#info').close();
+  $('#video-list').empty();
+  $('#title').text(tag);
+  $.ajax({
+    url: 'https://script.google.com/macros/s/AKfycbxJOWoWTM1lKPXX0J1aXAXgBHA1jBOFDaHQ1JLPeLrArbYsLGe9/exec?tag=' + tag,
+    type: 'GET'
+  })
+  .done((data) => {
+    console.log("succeded for getting data");
+    json = JSON.parse(data);
+    $('#title').append(' [' + json.length + ']');
+    for (var i in json) {
+      var a = document.createElement('a');
+      a.href = '#';
+      a.setAttribute('onclick', 'loadPlayer(' + i + ')');
+      a.classList.add('relative');
+      var img = document.createElement('img');
+      img.src = json[i].thumbnail;
+      img.classList.add('thumbnail');
+      a.append(img);
+      $('#video-list').append(a); 
+      // $('#video-list img').eq(i).attr('src', json[i].thumbnail);
+    }
+    // loadPlayer(0);
+  })
+  .fail((data) => {
+    console.log("failed for getting data");
+  })
+  .always((data) => {
+  });
+}
+
 
 $("#video-list > a").click(function () {
   console.log("from click a");
@@ -48,11 +85,58 @@ function loadPlayer(id) {
   $('.loader').remove();
   $('<img>', {
     class: 'loader',
-    src: 'img/loader.gif',
+    src: 'img/loader_pause.gif',
   }).appendTo($('#video-list a').eq(id));
 
+  $('#ons-list-info').empty();
+  var html = $('#ons-list-info');
+  html.append('<ons-list-item>Playing Video Info.</ons-list-item>');
+  if (json[id].title) {
+    html.append('<ons-list-header class="menu_header">Title</ons-list-header>');
+    html.append('<ons-list-item>' + json[id].title + '</ons-list-item>');
+  }
+  if (json[id].year) {
+    html.append('<ons-list-header class="menu_header">Filming Date</ons-list-header>');
+    html.append('<ons-list-item>' + json[id].year + '/' + json[id].month + '</ons-list-item>');
+  }
+  if (json[id].creator_name_en) {
+    html.append('<ons-list-header class="menu_header">Creator</ons-list-header>');
+    var item = $('<ons-list-item>', {
+      tappable: '',
+      onclick: 'updateVideo("' + json[id].creator_name_en + '")',
+    });
+    item.text(json[id].creator_name_en);
+    html.append(item);
+  }
+
+  html.append('<ons-list-header class="menu_header">Tag</ons-list-header>');
+  var tags = [];
+  if (json[id].country_name_en) {
+    tags.push(json[id].country_name_en);
+  }
+  if (json[id].area_name_en) {
+    tags.push(json[id].area_name_en);
+  }
+  if (json[id].spot_name_en) {
+    tags.push(json[id].spot_name_en);
+  }
+  if (json[id].sea_name_en) {
+    tags.push(json[id].sea_name_en);
+  }
+  if (json[id].other_name_en) {
+    tags.push(json[id].other_name_en);
+  }
+  for (var tag of tags) {
+    var item = $('<ons-list-item>', {
+      tappable: '',
+      onclick: 'updateVideo("' + tag + '")',
+    });
+    item.text(tag);
+    html.append(item);    
+  }
+  
   currentVideo = id;
-  console.log("loading video id " + id);
+  console.log("loading video" + json[id].preview_url);
   console.log(json[id].end);
   if (player) {
     player.destroy();
@@ -68,14 +152,13 @@ function loadPlayer(id) {
     playerVars: {
       start: json[id].start,
       end: json[id].end,
-      autoplay: 1,
       rel: 0,
       showinfo: 0,
       playsinline: 1,
       controls: 1,
-      modestbranding: 1, 
+      modestbranding: 1,
       disablekb: 1,
-      enablejsapi: 1,
+      enablejsapi: 0,
     }
   });
 }
@@ -92,15 +175,16 @@ var done = false;
 function onPlayerStateChange(event) {
   // 再生した時
   if (event.data == YT.PlayerState.PLAYING && !done) {
-    // setTimeout(stopVideo, 6000);
-    // done = true;
+    $('.loader').attr('src','img/loader.gif');    
   } else if (event.data == YT.PlayerState.ENDED) {
     nextVideo();
   }
 }
+
+// 音楽プレイヤー用
 function playVideo() {
   player.playVideo();
-  $('.loader').attr('src','img/loader.gif');
+  $('.loader').attr('src','img/loader_pause.gif');
   console.log(player.getCurrentTime());
 }
 function pauseVideo() {
--- www/js/controller.js
+++ www/js/controller.js
@@ -7,19 +7,11 @@ document.addEventListener('init', function (event) {
         page.querySelector('#menu_button').onclick = function () {
             document.querySelector('#menu').open();
         };
+        page.querySelector('#info_button').onclick = function () {
+            document.querySelector('#info').open();
+        };
     }
     else if (page.id === 'movie_details') {
         page.querySelector('#movie_title').innerHTML = page.data.title;
     }
 });
-function openMovieDetails(id) {
-    document.querySelector('#myNavigator').pushPage('movie_details.html', { data: { title: id } });
-}
-function openHome() {
-    document.querySelector('#myNavigator').pushPage('home_splitter.html');
-}
-function goBack() {
-    document.querySelector('#menu').close().then(function () {
-        document.querySelector('#myNavigator').popPage();
-    });
-}
